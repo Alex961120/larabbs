39512c2d991a3a1f088fa1257ad2331a7f35d0ba
Alex961120 <805119233@qq.com>
2018-10-09 09:46:19 +0000

    Keep track of user last visited time

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
 
