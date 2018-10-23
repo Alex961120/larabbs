250798d6fda16e35fbad9e839c25ea3e2ac52e4b
Alex961120 <805119233@qq.com>
2018-10-23 14:39:39 +0000

    topic index

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
-use App\Transformers\TopicsTransformer;
+use App\Models\User;
+use App\Transformers\TopicTransformer;
 use Illuminate\Http\Request;
 
 class TopicsController extends Controller
@@ -15,7 +16,7 @@ class TopicsController extends Controller
         $topic->user_id = $this->user()->id;
         $topic->save();
 
-        return $this->response->item($topic, new TopicsTransformer())
+        return $this->response->item($topic, new TopicTransformer())
             ->setStatusCode(201);
     }
 
@@ -24,7 +25,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
 
         $topic->update($request->all());
-        return $this->response->item($topic, new TopicsTransformer());
+        return $this->response->item($topic, new TopicTransformer());
     }
 
     public function destroy(Topic $topic)
@@ -33,5 +34,36 @@ class TopicsController extends Controller
 
         $topic->delete();
         return $this->response->noContent();
+    }
+
+    public function index(Request $request, Topic $topic)
+    {
+        $query = $topic->query();
+
+        if ($categoryId = $request->category_id) {
+            $query->where('category_id', $categoryId);
+        }
+
+        switch ($request->order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
+    public function userIndex(User $user, Request $request)
+    {
+        $topics = $user->topics()->recent()
+            ->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,12 @@ $api->version('v1', [
         // 分类列表
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 话题列表
+        $api->get('topics', 'TopicsController@index')
+            ->name('api.topics.index');
+        // 某用户发布的话题
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
 
         // 需要 token 验证的接口
         // 刷新token
