48c4a6d125e7db77b6e286d8c827fbe815133699
Alex961120 <805119233@qq.com>
2018-10-25 10:19:40 +0000

    replies store

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Requests\Api;
 
-use Dingo\Api\Http\FormRequest;
 
 class UserRequest extends FormRequest
 {
-    public function authorize()
-    {
-        return true;
-    }
-
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
     public function rules()
     {
         switch($this->method()) {
