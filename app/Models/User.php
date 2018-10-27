09131b7dfffb941d0ea989d903737b852db2ed94
Alex961120 <805119233@qq.com>
2018-10-27 03:30:31 +0000

    push notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,7 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $fillable = [
         'name', 'phone', 'email', 'password', 'avatar', 'introduction',
-        'weixin_openid', 'weixin_unionid'
+        'weixin_openid', 'weixin_unionid', 'registration_id'
     ];
 
     /**
@@ -66,7 +66,7 @@ class User extends Authenticatable implements JWTSubject
             // 拼接完整的 URL
             $path = config('app.url') . "/uploads/images/avatar/$path";
         }
-        
+
         $this->attributes['avatar'] = $path;
     }
 
